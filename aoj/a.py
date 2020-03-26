8f547baaa5e1219d57362ae2332a69484d6d0d63
uk-ar <yuuki.ari@gmail.com>
2020-03-27 05:03:16 +0900

    1_7_c

--- aoj/a.py
+++ aoj/a.py
@@ -8,46 +8,49 @@ class Node:
     self.r = -1 # right sibling
 
 T = [Node() for _ in range(n)]
-D = [0]*n
 sys.setrecursionlimit(1500)
-def rec(u,p):
-  D[u] = p
-  if T[u].r != -1: rec(T[u].r,p)
-  if T[u].l != -1: rec(T[u].l,p+1)
-
-H = [0]*n
-def rec1(u):
-  r = l = 0
-  if T[u].r != -1: r = rec(T[u].r)
-  if T[u].l != -1: l = rec(T[u].l)
-  H[u] = max(r,l)
-  return H[u]
 
 for i in range(n):
     v,l,r = map(int,input().split())
     T[v].r = r
-    if T[v].p != -1 and T[v].r != -1:
-      T[r].p = T[v].p
-    c = T[v].l = l
-    print(i)
-    while c != -1:
-      T[c].p = v
-      c = T[c].r
+    if r != -1:
+      T[r].p = v
+    T[v].l = l
+    if l != -1:
+      T[l].p = v
 
 for i in range(n):
+  #print(i,T[i].p,T[i].l,T[i].r)
   if T[i].p == -1 : r = i
 
-rec(r,0)
-rec1(r)
-for i in range(n):
-  if T[i].p == -1:
-    kind = "root"
-  elif T[i].l != None: kind = "internal node"
-  else: kind = "leaf"
-  d = 0
-  n = T[i].l
-  while n != None:
-    d += 1
-    n = T[n].r
-  print("node {}: parent = {}, sibling = {}, degree = {}, depth = {}, height = {}, {}"
-        .format(i,T[i].p,T[i].r,d,D[i],H[i],kind))
+ret = []
+def preorder(v):
+  if v == -1: return
+  ret.append(v)
+  preorder(T[v].l)
+  preorder(T[v].r)
+
+def inorder(v):
+  if v == -1: return
+  inorder(T[v].l)
+  ret.append(v)
+  inorder(T[v].r)
+
+def postorder(v):
+  if v == -1: return
+  postorder(T[v].l)
+  postorder(T[v].r)
+  ret.append(v)
+
+ret = []
+preorder(r)
+print("Preorder")
+print(""," ".join(map(str,ret)))
+ret = []
+inorder(r)
+print("Inorder")
+print(""," ".join(map(str,ret)))
+ret = []
+postorder(r)
+print("Postorder")
+print(""," ".join(map(str,ret)))
