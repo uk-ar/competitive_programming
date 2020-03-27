8f7ae2bc73c7443d349adf0dfea537a05152ffae
uk-ar <yuuki.ari@gmail.com>
2020-03-28 02:08:31 +0900

    1_9_b

--- aoj/alds1_9_a.py
+++ aoj/alds1_9_a.py
@@ -2,19 +2,19 @@
 import sys
 sys.setrecursionlimit(15000)
 n = int(input())
-A = list(map(int,input().split()))
+A = [0]+list(map(int,input().split()))
 
-for i in range(n):
-  if i != 0:
-    parent = "parent key = {}, ".format(str(A[(i-1)//2]))
+for i in range(1,n+1):
+  if i != 1:
+    parent = "parent key = {}, ".format(str(A[(i)//2]))
   else:
     parent = ""
-  if i*2+1 < n:
-    left = "left key = {}, ".format(str(A[i*2+1]))
+  if i*2 < n+1:
+    left = "left key = {}, ".format(str(A[i*2]))
   else:
     left = ""
-  if i*2+2 < n:
-    right = "right key = {}, ".format(str(A[i*2+2]))
+  if i*2+1 < n+1:
+    right = "right key = {}, ".format(str(A[i*2+1]))
   else:
     right = ""
-  print("node {}: key = {}, {}{}{}".format(i+1,str(A[i]),parent,left,right))
+  print("node {}: key = {}, {}{}{}".format(i,str(A[i]),parent,left,right))
