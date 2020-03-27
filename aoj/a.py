3d6795da6f719e19e2c1d604b0f85994c9066e86
uk-ar <yuuki.ari@gmail.com>
2020-03-27 20:50:40 +0900

    1_8_c

--- aoj/a.py
+++ aoj/a.py
@@ -8,7 +8,7 @@ class Node:
     self.left = None
     self.right = None
 
-sys.setrecursionlimit(1500)
+sys.setrecursionlimit(15000)
 r = None
 def insert(z):
   global r
@@ -43,24 +43,58 @@ def preorder(r):
   preorder(r.right)
 
 def search(r,key):
-  if r == None: return
+  if r == None: return None
   if r.key == key:
-    return True
+    return r
   if r.key < key:
     return search(r.right,key)
   else:
     return search(r.left,key)
 
+def delete(key):
+  global r
+  z = search(r,key)
+  if not z.left or not z.right:
+    y = z # node to delete
+  else:
+    y = z.right
+    while y.left:
+      y = y.left
+
+  if y.left:
+    x = y.left # child node of delete node
+  else:
+    x = y.right
+
+  if x:
+    x.p = y.p
+
+  if y.p == None:
+    r = x
+  elif y == y.p.left:
+    y.p.left = x
+  else:
+    y.p.right = x
+
+  if y != z:
+    z.key = y.key
+
 for i in range(n):
   s = input()
+  #print(s)
   if s[0]=="i":
     insert(Node(int(s.split()[1])))
   elif s[0]=="f":
-    if search(r,int(s.split()[1])):
+    if search(r,int(s.split()[1])) != None:
       print("yes")
     else:
       print("no")
+  elif s[0]=="d":
+    #print("d",int(s.split()[1]))
+    #print(r.key)
+    delete(int(s.split()[1]))
   else:
+    #print("p")
     ret = []
     inorder(r)
     print(""," ".join(map(str,ret)))
