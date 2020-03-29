d7b0efd30ef48c94782a2fe246373c20ed8f8040
uk-ar <yuuki.ari@gmail.com>
2020-03-29 21:35:59 +0900

    abc160

--- aoj/a.py
+++ aoj/a.py
@@ -5,25 +5,25 @@ n = int(sys.stdin.readline())
 
 ret = []
 inp = sys.stdin.readlines()
-G = [None]*(n)
-d = [0]*(n)
-f = [0]*(n)
-def dfs(i,t):
-  if d[i] != 0:
-    return t
-  t = t+1
-  d[i] = t
-  for j in G[i]:
-    t = dfs(j,t)
-  f[i] = t+1
-  return f[i]
-for i in range(n):
-  v = list(map(int,inp[i].split()))
-  G[v[0]-1] = list(map(lambda x: int(x-1),v[2:]))
-#print(G)
-t = 0
-for j in range(n):
-  t = dfs(j,t)
+G = [None]*(n+1)
+depths = [-1]*(n+1)
 
-for i in range(n):
-  print(i+1,d[i],f[i])
+for i in range(1,n+1):
+  v = list(map(int,inp[i-1].split()))
+  G[v[0]] = list(map(int,v[2:]))
+#print(G)
+
+nodes = [1]
+depth = 0
+while nodes:
+  new = []
+  for i in nodes:
+    if depths[i] == -1:
+      depths[i] = depth
+      new += G[i]
+    #print(i,nodes,depth,depths)
+  depth += 1
+  nodes = new
+
+for i in range(1,n+1):
+  print(i,depths[i])
