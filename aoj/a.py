1e29702eba9716e515db560eba42d1fe53f6e7c4
uk-ar <yuuki.ari@gmail.com>
2020-04-01 21:43:59 +0900

    1_12_a

--- aoj/a.py
+++ aoj/a.py
@@ -2,20 +2,21 @@
 import sys
 sys.setrecursionlimit(15000)
 n = int(sys.stdin.readline())
-G = [list(map(int,l.split())) for l in sys.stdin.readlines()]
+M = [list(map(int,l.split())) for l in sys.stdin.readlines()]
 
-s = {0}
-su = 0
-while len(s) < n:
-  mi = 0
-  mj = 0
-  for i in s:
-    for j in range(n):
-      if G[i][j] == -1 or j in s:
-        continue
-      if (mi == 0 and mj == 0) or G[i][j] < G[mi][mj]:
-        mi = i
-        mj = j
-  s.add(mj)
-  su += G[mi][mj]
-print(su)
+color = set()
+d = [float("inf")]*n
+d[0]=0
+p = [-1]*n
+#print(M)
+while len(color) < n:
+  u = -1
+  for i in range(n):
+    if not i in color and (u == -1 or d[u] > d[i]):
+      u = i
+  color.add(u)
+  for v in range(n):
+    if not v in color and M[u][v] != -1 and d[v] > M[u][v]:
+      d[v] = M[u][v]
+      p[v] = u
+print(sum(M[i][p[i]] for i in range(1,n)))
