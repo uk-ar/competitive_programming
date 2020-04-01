45abe106cb3b1495d9d6f8a0160f13a724569371
uk-ar <yuuki.ari@gmail.com>
2020-04-02 06:41:25 +0900

    alds_1_3_d

--- aoj/a.py
+++ aoj/a.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 # n,m = map(int,sys.stdin.readline().split())
 # a = list(map(int,sys.stdin.readline().split()))
+# s = sys.stdin.readline().rstrip()
 import sys,bisect
 sys.setrecursionlimit(15000)
-h,w = map(int,sys.stdin.readline().split())
-dp = [[0]*w for _ in range(h)]
-m = 0
-for i in range(h):
-  a = list(map(int,sys.stdin.readline().split()))
-  for j in range(w):
-    if a[j] == 1:
-      dp[i][j] = 0
-    elif i == 0 or j == 0:
-      dp[i][j] = 1
-      m = max(m,dp[i][j])
-    else:
-      dp[i][j] = min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])+1
-      m = max(m,dp[i][j])
-print(m*m)
+s = sys.stdin.readline().rstrip()
+st = []
+ars = []
+ar = 0
+for i,c in enumerate(s):
+  if c == "\\":
+    st.append(i)
+  elif c == "/":
+    if st:
+      j = st.pop()
+      ar += i-j
+      while ars and j < ars[-1][1]:
+        ar0,_ = ars.pop()
+        ar += ar0
+      ars.append([ar,j])
+      ar = 0
+print(sum([ar for ar,_ in ars]))
+if len(ars):
+  print(0)
+else:
+  print(len(ars)," ".join(map(str,[ar for ar,_ in ars])))
