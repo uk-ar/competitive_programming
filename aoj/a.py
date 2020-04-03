5c7b074ca26f0f63cb1c28ece402a60c04ecab15
uk-ar <yuuki.ari@gmail.com>
2020-04-03 21:07:51 +0900

    alds_1_3_d

--- aoj/a.py
+++ aoj/a.py
@@ -4,24 +4,16 @@
 # s = sys.stdin.readline().rstrip()
 import sys,bisect
 sys.setrecursionlimit(15000)
-s = sys.stdin.readline().rstrip()
-st = []
-ars = []
-ar = 0
-for i,c in enumerate(s):
-  if c == "\\":
-    st.append(i)
-  elif c == "/":
-    if st:
-      j = st.pop()
-      ar += i-j
-      while ars and j < ars[-1][1]:
-        ar0,_ = ars.pop()
-        ar += ar0
-      ars.append([ar,j])
-      ar = 0
-print(sum([ar for ar,_ in ars]))
-if len(ars):
-  print(0)
-else:
-  print(len(ars)," ".join(map(str,[ar for ar,_ in ars])))
+h,w = map(int,sys.stdin.readline().split())
+c = [None]*h
+
+for r in range(h):
+  a = list(map(int,sys.stdin.readline().split()))
+  for col in range(w):
+    if a[col] == 1:
+      c[r][col] = 0
+    elif r == 1:
+      c[0][col] = 1
+    else:
+      c[r][col] = c[r-1][col]+1
+print(c)
