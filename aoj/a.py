8360e13288ab5eef4c9b3fbedcb6b2027a1ca0bf
uk-ar <yuuki.ari@gmail.com>
2020-04-08 03:15:32 +0900

    alds_1_3_b passed

--- aoj/a.py
+++ aoj/a.py
@@ -5,40 +5,32 @@
 import sys,bisect
 sys.setrecursionlimit(15000)
 h,w = map(int,sys.stdin.readline().split())
-c = [[0]*(w+1) for _ in range(h)]
+buffer = [None for _ in range(h)]
+T = [[0]*(w+1) for _ in range(h)]
 
 for r in range(h):
-  a = list(map(int,sys.stdin.readline().split()))
+  buffer = list(map(int,sys.stdin.readline().split()))
   for col in range(w):
-    if a[col] == 1:
-      c[r][col] = 0
+    if buffer[col] == 1:
+      T[r][col] = 0
     elif r == 0:
-      c[0][col] = 1
+      T[0][col] = 1
     else:
-      c[r][col] = c[r-1][col]+1
+      T[r][col] = T[r-1][col]+1
 m = 0
 
-for r,row in enumerate(c):
-  st = [{"h":0,"p":-1}]
+for buffer in T:
+  stack = []
   for i in range(w+1):
-    n = {"h":row[i],"p":i}
-    if not st:
-      st.append(n)
-    elif st[-1]["h"] < row[i]:
-      st.append(n)
-    elif st[-1]["h"] == row[i]:
+    if not stack:
+      stack.append({"h":buffer[i],"p":i})
+    elif stack[-1]["h"] < buffer[i]:
+      stack.append({"h":buffer[i],"p":i})
+    elif stack[-1]["h"] == buffer[i]:
       continue
-    elif st[-1]["h"] > row[i]:
-      n = {"h":row[i],"p":st[-1]["p"]}
-      while st and st[-1]["h"] >= row[i]:
-        n = {"h":row[i],"p":st[-1]["p"]}
-        a = st.pop()
-        m = max(m,(i-a["p"])*a["h"])
-      if st and st[-1]["h"] < row[i]:
-        st.append(n)
-  if st:
-    a = st.pop()
-    m = max(m,(i-a["p"])*a["h"])
-    #if r == 8:
-    #   print(r,i,row,st,m)
+    elif stack[-1]["h"] > buffer[i]:
+      while stack and stack[-1]["h"] >= buffer[i]:
+        pre = stack.pop()
+        m = max(m,pre["h"]*(i-pre["p"]))
+      stack.append({"h":buffer[i],"p":pre["p"]})
 print(m)
