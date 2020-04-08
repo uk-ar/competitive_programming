e3f09b8422e6595c192f0c6534d87233b3a0c870
uk-ar <yuuki.ari@gmail.com>
2020-04-09 05:00:15 +0900

    alds_1_3_c

--- aoj/a.py
+++ aoj/a.py
@@ -2,35 +2,27 @@
 # n,m = map(int,sys.stdin.readline().split())
 # a = list(map(int,sys.stdin.readline().split()))
 # s = sys.stdin.readline().rstrip()
+# n = int(sys.stdin.readline())
 import sys,bisect
 sys.setrecursionlimit(15000)
-h,w = map(int,sys.stdin.readline().split())
-buffer = [None for _ in range(h)]
-T = [[0]*(w+1) for _ in range(h)]
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().split()))
+a.append(0)
 
-for r in range(h):
-  buffer = list(map(int,sys.stdin.readline().split()))
-  for col in range(w):
-    if buffer[col] == 1:
-      T[r][col] = 0
-    elif r == 0:
-      T[0][col] = 1
-    else:
-      T[r][col] = T[r-1][col]+1
-m = 0
-
-for buffer in T:
-  stack = []
-  for i in range(w+1):
-    if not stack:
-      stack.append({"h":buffer[i],"p":i})
-    elif stack[-1]["h"] < buffer[i]:
-      stack.append({"h":buffer[i],"p":i})
-    elif stack[-1]["h"] == buffer[i]:
-      continue
-    elif stack[-1]["h"] > buffer[i]:
-      while stack and stack[-1]["h"] >= buffer[i]:
-        pre = stack.pop()
-        m = max(m,pre["h"]*(i-pre["p"]))
-      stack.append({"h":buffer[i],"p":pre["p"]})
-print(m)
+st = []
+ar = 0
+for i in range(n+1):
+  if not st:
+    st.append([i,a[i]])
+  elif st[-1][1] < a[i]:
+    st.append([i,a[i]])
+  elif st[-1][1] == a[i]:
+    continue
+  elif st[-1][1] > a[i]:
+    while st and st[-1][1] >= a[i]:
+      j,h = st.pop()
+      ar = max(ar,(i-j)*(h))
+    st.append([j,a[i]])
+  #print(i,ar,st)
+#print(a)
+print(ar)
