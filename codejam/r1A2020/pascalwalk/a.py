28999363d826c86b3b4fc6302ee735bc79e44896
uk-ar <yuuki.ari@gmail.com>
2020-04-18 01:18:42 +0900

    jam pascal 438 steps

--- codejam/r1A2020/pascalwalk/a.py
+++ codejam/r1A2020/pascalwalk/a.py
@@ -17,47 +17,86 @@ def comb(n, r):
         fd[r-1] = math.factorial(r-1)
     return  fd[n-1] // (fd[n - r] * fd[r-1])
 
-def down(cur):
-    return [cur[0]+1,cur[1]+cur[0]%2]
+def down(cur,rest):
+    nex = [cur[0]+1,cur[1]+cur[0]%2]
+    com = comb(nex[0],nex[1])
+    return nex,rest - com
 
-def up(cur):
-    return [cur[0]-1,cur[1]-(cur[0]+1)%2]
+def up(cur,rest):
+    nex = [cur[0]-1,cur[1]-(cur[0]+1)%2]
+    com = comb(nex[0],nex[1])
+    return nex,rest - com
+
+def right_or_up(cur,rest):
+    right = [cur[0],cur[1]+1]
+    com = comb(right[0],right[1])
+    if cur[1] > 1 and cur[0] > 1:
+        nex = [cur[0]-1,cur[1]-1]
+    elif com + 1 <= rest:
+        nex = right
+    else:
+        nex = [cur[0]-1,cur[1]]
+    return nex,rest-comb(nex[0],nex[1])
+
+# def down(cur,rest):
+#     down = [cur[0]+1,cur[1]+1]
+#     com = comb(down[0],down[1])
+#     if cur[1] > 1:
+#         nex = [cur[0],cur[1]-1]
+#     elif com + 1 <= rest:
+#         nex = down
+#     else:
+#         nex = [cur[0]+1,cur[1]]
+#     return nex,rest-comb(nex[0],nex[1])
 
 def solve():
     n = int(sys.stdin.readline())
-    if n < 4:
+    if n < 500:
         ret = [[i,i] for i in range(1,n+1)]
     else:
-        ret = [[1,1],[2,2]]
-        cur = [2,2]
-        rest = n-2
-        buf = 1
+        ret = [[1,1],[2,2],[3,2],[4,3]]
+        cur = [4,3]
+        rest = n-7
+        buf = 3.3
         while rest > 0:
-            nex = down(cur)
+            nex,_ = down(cur,rest)
             com = comb(nex[0],nex[1])
             while rest > com*buf:
                 cur = nex
                 rest = rest - com
                 ret.append(cur)
-                nex = down(cur)
+                nex,_ = down(cur,rest)
                 com = comb(nex[0],nex[1])
                 buf = 3.3
-                #print(cur,comb(cur[0],cur[1]),nex,com,rest)
-            if cur[1] != 1:
+                print(cur,comb(cur[0],cur[1]),nex,com,rest)
+            if cur[1] > 3:
                 cur = [cur[0],cur[1]-1]#shift left
                 rest = rest - comb(cur[0],cur[1])
                 ret.append(cur)
-            while rest > 0 and cur[1] != 1:
-                cur = up(cur)
-                rest = rest - comb(cur[0],cur[1])
+            while rest > 0 and cur[1] > 3:#up
+                cur,rest = up(cur,rest)
                 ret.append(cur)
-                #print(cur,comb(cur[0],cur[1]),nex,com,rest)
+                print(cur,comb(cur[0],cur[1]),nex,com,rest)
             if rest > 0:
-                cur = [cur[0]+1,cur[1]]#shift up
-                rest = rest - comb(cur[0],cur[1])
-                ret.append(cur)
-                buf = 1
-            #print(cur,comb(cur[0],cur[1]),rest)
+                #print("n",n)
+                #if n > 1000 and (cur[0]-1)*(cur[0]-1)//2 > rest:
+                if (cur[0]-1)*(cur[0]-1)//2 > rest:
+                    while rest > 0 and cur[1] > 1:
+                        cur,rest = up(cur,rest)
+                        ret.append(cur)
+                    while rest > 0:
+                        cur,rest = right_or_up(cur,rest)
+                        ret.append(cur)
+                else:
+                    # while rest > 0 and cur[1] > 1:
+                    #     cur,rest = up(cur,rest)
+                    #     ret.append(cur)
+                    if rest > 0:
+                        cur = [cur[0]+1,cur[1]]#shift down
+                        rest = rest - comb(cur[0],cur[1])
+                        ret.append(cur)
+                    buf = 2
+            print(cur,comb(cur[0],cur[1]),rest)
     print(*[" ".join(map(str,p)) for p in ret],sep="\n")
     print(len({" ".join(map(str,p)) for p in ret}),len(ret))
 
