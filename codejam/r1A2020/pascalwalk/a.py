4d2f8cae40254bc0c8c0a8a5be7724983651808d
uk-ar <yuuki.ari@gmail.com>
2020-04-24 11:36:58 +0900

    pascalwalk full ok

--- codejam/r1A2020/pascalwalk/a.py
+++ codejam/r1A2020/pascalwalk/a.py
@@ -19,14 +19,14 @@ def comb(n, r):
 
 def down(cur,rest,ret):
     if rest == 0:
+        ret.append([cur,comb(cur[0],cur[1]),rest])
         return
     nex = [cur[0]+1,cur[1]+cur[0]%2]
     com = comb(nex[0],nex[1])
-    lim = com * 3.3
+    lim = com * 3.4
     # if cur[1]==3:
     #     lim = com+cur[0]*2
-    #print("d",cur,comb(nex[0],nex[1]),rest)
-    #print(cur)
+    #print("d",cur,comb(cur[0],cur[1]),rest)
     if rest > lim:
         ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
         down(nex,rest-com,ret)
@@ -37,10 +37,10 @@ def down(cur,rest,ret):
         com = comb(nex[0],nex[1])
         ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
         up(nex,rest-com,ret)
-    else:
+    else:# cur[1]==3
         nex = [cur[0]+1,cur[1]]#down
         com = comb(nex[0],nex[1])
-        if rest > com+cur[0]*2:
+        if rest > com+cur[0]+2:
             ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
             down(nex,rest-com,ret)
         else:
@@ -49,8 +49,8 @@ def down(cur,rest,ret):
 
 def up(cur,rest,ret):
     if rest == 0:
+        ret.append([cur,comb(cur[0],cur[1]),rest])
         return
-    steps = 500-len(ret)
     #print("u",cur,comb(cur[0],cur[1]),rest)
     if cur[1] > 3:
         nex = [cur[0]-1,cur[1]-(cur[0]+1)%2]
@@ -65,6 +65,7 @@ def up(cur,rest,ret):
 
 def left(cur,rest,ret):
     if rest == 0:
+        ret.append([cur,comb(cur[0],cur[1]),rest])
         return
     #print("l",cur,comb(cur[0],cur[1]),rest)
     if cur[1] > 2:
@@ -72,28 +73,41 @@ def left(cur,rest,ret):
         com = comb(nex[0],nex[1])
         ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
         left(nex,rest-com,ret)
-    else:
-        #steps = 500-len(ret)
-        #if rest < cur[0]*cur[0]//2:
-        if cur[0]>50:
-            nex = [cur[0]-1,cur[1]]#up
-            com = comb(nex[0],nex[1])
-            ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
-            right_or_up(nex,rest-com,ret)
-        else:
-            left_or_down(cur,rest,ret)
+    elif rest < cur[0]*2:# cur[1] == 2
+        nex = [cur[0],cur[1]-1]#left
+        com = comb(nex[0],nex[1])
+        ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
+        left_or_down(nex,rest-com,ret)
+    elif cur[0] > 4:# cur[1] == 2
+        #if cur[0]>40:
+        nex = [cur[0]-1,cur[1]]#up
+        com = comb(nex[0],nex[1])
+        ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
+        right_or_up(nex,rest-com,ret)
+    else:# cur[1] == 2 && cur[1] < 4
+        #if cur[0]>40:
+        nex = [cur[0],cur[1]-1]#left
+        com = comb(nex[0],nex[1])
+        ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
+        left_or_down(nex,rest-com,ret)
+        # else:
+        #     nex = [cur[0],cur[1]-1]#left
+        #     com = comb(nex[0],nex[1])
+        #     ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
+        #     left_or_down(nex,rest-com,ret)
 
 def right_or_up(cur,rest,ret):
     if rest == 0:
+        ret.append([cur,comb(cur[0],cur[1]),rest])
         return
-    nex = [cur[0]-1,cur[1]]
+    nex = [cur[0]-1,cur[1]]#up
     com = comb(nex[0],nex[1])
     #print("ru",cur,comb(nex[0],nex[1]),rest)
-    if cur[1] == 2 and rest > com:
+    if cur[1] == 2 and rest-com > 0 and cur[0]>4:
         ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
         right_or_up(nex,rest-com,ret)
     elif cur[1] == 2:
-        nex = [cur[0],cur[1]-1]
+        nex = [cur[0],cur[1]-1]#right
         com = comb(nex[0],nex[1])
         ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
         right_or_up(nex,rest-com,ret)
@@ -116,25 +130,27 @@ def right_or_up(cur,rest,ret):
 
 def left_or_down(cur,rest,ret):
     if rest == 0:
+        ret.append([cur,comb(cur[0],cur[1]),rest])
         return
     down = [cur[0]+1,cur[1]+1]
     com = comb(down[0],down[1])
-    if cur[1] > 1:
-        nex = [cur[0],cur[1]-1]
-    elif com + 1 <= rest:
-        nex = down
-    else:
-        nex = [cur[0]+1,cur[1]]
+    # if cur[1] > 1:
+    #     nex = [cur[0],cur[1]-1]
+    # elif com + 1 <= rest:
+    #     nex = down
+    # else:
+    nex = [cur[0]+1,cur[1]]
     com = comb(nex[0],nex[1])
+    #print("ld",cur,comb(nex[0],nex[1]),rest)
     ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
     left_or_down(nex,rest-com,ret)
 
 def solve():
     n = int(sys.stdin.readline())
-    if n < 501:
-        ret = [[[i,i]] for i in range(1,n+1)]
+    if n < 18:
+        ret = [[[i,i],0] for i in range(1,n+1)]
     else:
-        ret = [[[1,1]],[[2,2]],[[3,2]]]
+        ret = [[[1,1]],[[2,2]],[[3,2],0]]
         cur = [4,3]
         rest = n-7
         buf = 3.3
@@ -173,9 +189,12 @@ def solve():
         #                 ret.append([cur,comb(cur[0],cur[1]),nex,com,rest])
         #             buf = 2
             #print(cur,comb(cur[0],cur[1]),rest)
-    #print(*[" ".join(map(str,p[0])) for p in ret],sep="\n")
-    print(ret[-1])
-    print(len({" ".join(map(str,p[0])) for p in ret}),len(ret))
+    print(*[" ".join(map(str,p[0])) for p in ret],sep="\n")
+    #print(*[" ".join(map(str,p)) for p in ret],sep="\n")
+    #assert(ret[-1][-1]==0)
+    #print(ret[-1][-1])
+    #assert(len({" ".join(map(str,p[0])) for p in ret})==len(ret))
+    #assert(len(ret)<=500)
 
 T = int(sys.stdin.readline())
 for t in range(T):
