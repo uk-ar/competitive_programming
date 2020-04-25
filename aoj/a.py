ffba82859e348ce6d40bbb63123e92a46134cb54
uk-ar <yuuki.ari@gmail.com>
2020-04-25 16:30:35 +0900

    dpl_1_e

--- aoj/a.py
+++ aoj/a.py
@@ -11,14 +11,18 @@ s1 = sys.stdin.readline().rstrip()
 s2 = sys.stdin.readline().rstrip()
 l1 = len(s1)
 l2 = len(s2)
-dp = [[float("inf")]*l2 for _ in range(l1)]
-dp[0][0] = 0
+dp = [[float("inf")]*(l2+1) for _ in range(l1+1)]
+dp[0] = list(range(l2+1))
 
 for i in range(l1):
+  dp[i][0] = i
   for j in range(l2):
     if s1[i]==s2[j]:
-      dp[i][j] = dp[max(i-1,0)][max(j-1,0)]
+      dp[i+1][j+1] = dp[i][j]
     else:
-      dp[i][j] = min(dp[max(i-1,0)][j]+1,dp[i][max(j-1,0)]+1,dp[max(i-1,0)][max(j-1,0)]+1)
+      dp[i+1][j+1] = min(dp[i+1][j]+1,
+                         dp[i][j+1]+1,
+                         dp[i][j]+1)
 
+#print(dp)
 print(dp[-1][-1])
