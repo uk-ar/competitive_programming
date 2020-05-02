e1c72eb0860a69eb8fa78dea7603182784bc01f9
uk-ar <yuuki.ari@gmail.com>
2020-05-02 22:04:54 +0900

    1pass

--- abc165/d/a.py
+++ abc165/d/a.py
@@ -9,6 +9,13 @@ sys.setrecursionlimit(15000)
 
 A,B,N = map(int,sys.stdin.readline().split())
 
+# if A < B:
+#     x = B//2
+#     if x < N:
+#         m = math.floor(A*i/B)-A*math.floor(i/B)
+# else:
+#     m = 0
+# print(m)
 m = -float("inf")
 for i in range(N+1):
     res = math.floor(A*i/B)-A*math.floor(i/B)
