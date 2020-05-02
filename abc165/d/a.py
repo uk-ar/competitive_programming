de05a1de16b513b3eae8f56f6396ca201211e578
uk-ar <yuuki.ari@gmail.com>
2020-05-02 22:01:10 +0900

    1pass

---
+++ abc165/d/a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# n,m = map(int,sys.stdin.readline().split())
+# a = list(map(int,sys.stdin.readline().split()))
+# a = [sys.stdin.readline() for _ in range(n)]
+# s = sys.stdin.readline().rstrip()
+# n = int(sys.stdin.readline())
+import sys,bisect,math
+sys.setrecursionlimit(15000)
+
+A,B,N = map(int,sys.stdin.readline().split())
+
+m = -float("inf")
+for i in range(N+1):
+    res = math.floor(A*i/B)-A*math.floor(i/B)
+    if m < res:
+        print(i,res)
+        m = math.floor(A*i/B)-A*math.floor(i/B)
+print(m)
+# x = B//2
+# print(math.floor(A*x/B)-A*math.floor(x/B))
