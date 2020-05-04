c65030a9aa95db9723a8b59ad5bd0e2cf65c519c
uk-ar <yuuki.ari@gmail.com>
2020-05-05 05:44:18 +0900

    mod

--- aoj/dpl_1_c.py
+++ aoj/dpl_1_c.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 # n,m = map(int,sys.stdin.readline().split())
 # a = list(map(int,sys.stdin.readline().split()))
-# a = [sys.stdin.readline() for s in range(n)]
+# a = [list(map(int(sys.stdin.readline().split()))) for _ in range(N)]
 # s = sys.stdin.readline().rstrip()
 # n = int(sys.stdin.readline())
 import sys,bisect
 sys.setrecursionlimit(15000)
 
-st = []
-ar = 0
-while True:
-  n = int(sys.stdin.readline())
-  if n == 0:
-    break
-  a = [int(sys.stdin.readline()) for _ in range(n)]
-  ml = mg = a[0]
-  #print(a)
-  for i in range(1,n):
-    ml = max(ml+a[i],a[i])
-    mg = max(mg,ml)
-    #print(ml,mg,a[i])
-  print(mg)
+N,W = map(int,sys.stdin.readline().split())
+a = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+dp = [[0]*(W+1) for _ in range(N)]
+
+for i in range(N):
+  v,w = a[i]
+  for j in range(W+1):
+    if 0 <= j-w:
+      dp[i][j] = max(dp[i-1][j],dp[i][j-w]+v)
+    else:
+      dp[i][j] = dp[i-1][j]
+print(dp[-1][-1])
