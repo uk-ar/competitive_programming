a9d9ba4b57bd2a9819e429a2853136cc9b71280a
uk-ar <yuuki.ari@gmail.com>
2020-05-07 03:36:36 +0900

    sample passed dpl_1_g

--- aoj/dpl_1_c.py
+++ aoj/dpl_1_c.py
@@ -8,14 +8,21 @@ import sys,bisect
 sys.setrecursionlimit(15000)
 
 N,W = map(int,sys.stdin.readline().split())
-a = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-dp = [[0]*(W+1) for _ in range(N)]
+vw = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
-for i in range(N):
-  v,w = a[i]
+# #naive
+# dp = [[0]*(W+1) for _ in range(N)]
+# for v,w in vw:
+#   for j in range(W+1):
+#     if 0 <= j-w:
+#       dp[i][j] = max(dp[i-1][j],dp[i][j-w]+v)
+#     else:
+#       dp[i][j] = dp[i-1][j]
+# print(dp[-1][-1])
+# mem opt
+dp = [0]*(W+1)
+for v,w in vw:
   for j in range(W+1):
     if 0 <= j-w:
-      dp[i][j] = max(dp[i-1][j],dp[i][j-w]+v)
-    else:
-      dp[i][j] = dp[i-1][j]
-print(dp[-1][-1])
+      dp[j] = max(dp[j],dp[j-w]+v)
+print(dp[-1])
