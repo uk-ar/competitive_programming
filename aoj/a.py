a388c9a85fa4b505bbf7c70f4c4702948a956d72
uk-ar <yuuki.ari@gmail.com>
2020-05-06 09:07:21 +0900

    test pass but slow

--- aoj/a.py
+++ aoj/a.py
@@ -4,31 +4,50 @@
 # a = [sys.stdin.readline() for _ in range(n)]
 # s = sys.stdin.readline().rstrip()
 # n = int(sys.stdin.readline())
-INF = float("inf")
-import sys,bisect
-sys.setrecursionlimit(15000)
+import sys
 
-n = int(sys.stdin.readline())
-a = [int(sys.stdin.readline()) for _ in range(n)]
+N,W = map(int,sys.stdin.readline().split())
+a = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
-# naive O(n**2)
-# dp = [0]*n
-# for i in range(n):
-#   m = 0
-#   for j in range(0,i):
-#     if a[j] < a[i] and m <= dp[j]:
-#       m = dp[j]
-#   dp[i] = m+1
-# print(max(dp))
-import bisect
-dp = [float("inf")]*n
-dp[0] = a[0]
-last = 0
-for i in range(1,n):
-  if dp[last] < a[i]:
-    last += 1
-    dp[last] = a[i]
-  else:
-    j = bisect.bisect_left(dp,a[i])
-    dp[j] = a[i]
-print(bisect.bisect_left(dp,float("inf")))
+#for sparse data
+# 05_large 11s->6s
+import collections
+dp = {}
+dp[0] = 0
+a = sorted(a,reverse=True)
+for v,w in a:
+  temp = dp.copy()
+  for j in list(dp.keys()):
+    if j+w <= W and (not j+w in dp or dp[j+w] < dp[j]+v):
+      temp[j+w] = dp[j]+v
+  #dp = {**dp,**temp}
+  dp = temp
+print(max(dp.values()))
+
+# # naive
+# dp = [[0]*(W+1) for _ in range(N)]
+# for i in range(N):
+#   v,w = a[i]
+#   for j in range(W+1):
+#     if 0 <= j-w:
+#       dp[i][j] = max(dp[i-1][j],dp[i][j-w]+v)
+#     else:
+#       dp[i][j] = dp[i-1][j]
+# #opt mem
+# dp = [0]*(W+1)
+# for i in range(N):
+#   v,w = a[i]
+#   for j in range(W+1)[::-1]:
+#     if 0 <= j-w: #select
+#       dp[j] = max(dp[j],dp[j-w]+v)
+#opt
+# dp = [0]*(W+1)
+# last = 0
+# for i in range(N):
+#   v,w = a[i]
+#   # print(last)
+#   # print(dp)
+#   last = min(last+w+1, W+1)
+#   for j in range(last)[::-1]:
+#     if 0 <= j-w and dp[j] <= dp[j-w]+v: #select
+#       dp[j] = dp[j-w]+v
