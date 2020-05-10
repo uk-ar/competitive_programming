dbb919641798021ea75d6072f03a4da64cae1ae9
uk-ar <yuuki.ari@gmail.com>
2020-05-11 05:52:01 +0900

    all passed

--- abc167/c/a.py
+++ abc167/c/a.py
@@ -21,15 +21,17 @@ if any(e < X for e in ALL[1:]):
     print(-1)
     exit()
 
-dp = [[INF]*(M+1) for _ in range(N+1)]
-dp[0] = ALL
-for i in range(N+1):
-    dp[i] = dp[max(0,i-1)]
-    for j in range(i):
-        #print(i,j,dp[j],C[i-1],X)
-        # print(i,j,X,[(e2-e1) for e1,e2 in zip(C[j][1:],dp[j][1:])],
-        #              all((e2-e1)>=X for e1,e2 in zip(C[j][1:],dp[j][1:])))
-        if X < dp[j][0]-C[i-1][0] and dp[j][0]-C[i-1][0] < dp[i][0] and all((e2-e1)>=X for e1,e2 in zip(C[i-1][1:],dp[j][1:])):
-            dp[i] = [e3-e4 for e3,e4 in zip(dp[j],C[i-1])]
-#print(dp)
-print(dp[-1][0])
+m = INF
+def backtrack(n,c):
+    #print(n,c)
+    if n == N:
+        if all(a >= X for a in c[1:]):
+            #print("OK",n,c,X)
+            global m
+            m = min(m,c[0])
+        return
+    backtrack(n+1,c)#not select
+    backtrack(n+1,[a+b for a,b in zip(c,C[n])])#select
+
+backtrack(0,[0]*(M+1))
+print(m)
