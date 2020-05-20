33ecd0e6d5d44d7ca7305a0207adccf22b381583
uk-ar <yuuki.ari@gmail.com>
2020-05-20 22:33:59 +0900

    grl_3_c

--- aoj/grl_3_c.py
+++ aoj/grl_3_c.py
@@ -14,36 +14,45 @@ INF = float("inf")
 
 V,E = map(int,sys.stdin.readline().split())
 st = tuple(tuple(map(int,sys.stdin.readline().rstrip().split())) for _ in range(E)) # multi line with multi param
+Q = int(sys.stdin.readline())
+uv = tuple(tuple(map(int,sys.stdin.readline().rstrip().split())) for _ in range(Q)) # multi line with multi param
 #st = [[0,1],[0,2],[1,2],[2,3]]
+#uv = [[0,1],[0,2],[1,2],[2,3]]
 G = {i:[] for i in range(V)}
+rG = {i:[] for i in range(V)}
 
 for s,t in st:
     G[s].append(t)
+    rG[t].append(s)
 
-def dfs_cycle(s):
-    act.add(s)
-    visited.add(s)
-    #print(s,act,visited,G)
-    if not s in G:
-        act.remove(s)
-        return
-    for e in G[s]:
-        #print(s,e,act)
-        if e in act:
-            global bad
-            bad = True
-            return
-        elif not e in visited:
-            dfs_cycle(e)
-    act.remove(s)
+def dfs(current):
+    visited.add(current)
+    for nex in G[current]:
+        if not nex in visited: dfs(nex)
+    post_order.append(current)
 
-bad = False
+def rdfs(current,k):
+    visited.add(current)
+    topological_order[current]=k
+    for nex in rG[current]:
+        if not nex in visited: rdfs(nex,k)
+
 visited = set()
-act = set()
-for s,_ in st:
-    if not s in visited:
-        dfs_cycle(s)
-if bad:
-    print(1)#has cycle
-else:
-    print(0)
+post_order = []
+for i in range(V):
+    if not i in visited: dfs(i)
+
+topological_order = [0]*V
+visited = set()
+
+k = 0
+for i in post_order[::-1]:
+    if not i in visited:
+        rdfs(i,k)
+        k += 1
+
+for u,v in uv:
+    if topological_order[u] == topological_order[v]:
+        print(1)
+    else:
+        print(0)
