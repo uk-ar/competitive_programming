01935a07447a4dc1e76b531a9aba4853c0159287
uk-ar <yuuki.ari@gmail.com>
2020-05-31 07:28:56 +0900

    sample half passed

---
+++ nomura2020/b/a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# n,m = map(int,sys.stdin.readline().split())
+# a = tuple(map(int,sys.stdin.readline().split())) # single line with multi param
+# a = tuple(sys.stdin.readline() for _ in range(n)) # multi line with single param
+# a = tuple(tuple(map(int,sys.stdin.readline().rstrip().split())) for _ in range(N)) # multi line with multi param
+# s = sys.stdin.readline().rstrip()
+# n = int(sys.stdin.readline())
+# INF = float("inf")
+import sys,collections
+
+t = sys.stdin.readline().rstrip()
+print(t.replace('?','D'))
