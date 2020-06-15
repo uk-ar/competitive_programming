e70083bbadf1c7c6795b6bd010dca13a428a15b1
uk-ar <yuuki.ari@gmail.com>
2020-06-16 02:02:33 +0900

    passed

--- tkppc3/d/a.py
+++ tkppc3/d/a.py
@@ -15,19 +15,20 @@ a = tuple(map(int,sys.stdin.readline().split())) # single line with multi param
 b = tuple(map(int,sys.stdin.readline().split())) # single line with multi param
 q = tuple(tuple(map(int,sys.stdin.readline().rstrip().split())) for _ in range(Q)) # multi line with multi param
 
-cum = [[0]*(W+1) for _ in range(H+1)]
-base = [0]*(W+1)
+B = [0]*(W+1)
 
 sign = 1
 for c in range(W):
-    base[c+1] += base[c]+sign*b[c]
+    B[c+1] += B[c]+sign*b[c]
     sign *= -1
 
 sign = 1
+A = [0]*(H+1)
 for r in range(H):
-    for c in range(W+1):
-        cum[r+1][c] += cum[r][c]+sign*base[c]*a[r]
+    A[r+1] += A[r]+sign*a[r]
     sign *= -1
 
-for px,py,qx,qy in q:
-    print(cum[qx][qy]-cum[px-1][qy]-cum[qx][py-1]+cum[px-1][py-1])
+ans=[0]*Q
+for i in range(Q):
+    px,py,qx,qy = q[i]
+    print((A[qx]-A[px-1])*(B[qy]-B[py-1]))
