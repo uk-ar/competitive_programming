6f0c9b58afec436dbee7c2b6512d6b2198234b54
uk-ar <yuuki.ari@gmail.com>
2020-07-05 10:00:47 +0900

    passed

--- aoj/p2444.py
+++ aoj/p2444.py
@@ -7,58 +7,23 @@
 # N = int(sys.stdin.readline())
 # INF = float("inf")
 import sys
-
-N,M = map(int,sys.stdin.readline().split())
-s = sys.stdin.readline().rstrip()
-Q = tuple(sys.stdin.readline().rstrip() for _ in range(M)) # multi line with single param
-
+T = sys.stdin.readline().rstrip()
+NT = len(T)
+P = sys.stdin.readline().rstrip()
+NP = len(P)
+if NT < NP:
+    exit()
 #https://qiita.com/keymoon/items/11fac5627672a6d6a9f6
 MOD = 2**61-1 #prime
-A = [ord(c) - ord('a')+1 for c in s]
-pows = [1]*(N+1)
-shash = [0]*(N+1)
-for i in range(N):
-    pows[i+1] = (pows[i]*27)%MOD
-    shash[i+1] = (shash[i]*27+A[i])%MOD
-#print(shash)
-l = 0
-r = 0
-d = set()
-h = A[l]
-for q in Q:
-    if q == "L++":
-        #print(r-l,A[l],h-A[l]*pow(26,r-l,MOD))
-        #h = (h-A[l]*pows[r-l])%MOD
-        l += 1
-    elif q == "L--":
-        l -= 1
-        #h = (h+A[l]*pows[r-l])%MOD
-    elif q == "R++":
-        r += 1
-        #print(A[r],r)
-        #h = ((h*27)+A[r])%MOD
-    elif q == "R--":
-        r -= 1
-        #h = h//27
-    #print(h,r,l,s[l:r+1],shash[r+1]-shash[l]*pows[r+1-l])
-    #assert(h==(shash[r+1]-shash[l]*pows[r+1-l])%MOD)
-    #print(s[l:r+1],h,q)
-    assert(l<=r)
-    assert(0<=l)
-    assert(0<=r)
-    d.add((shash[r+1]-shash[l]*pows[r+1-l])%MOD)
-#print(d)
-print(len(d))
-# #https://qiita.com/keymoon/items/11fac5627672a6d6a9f6
-# MOD = 2**61-1 #prime
-# A = [ord(c) for c in T]
-# pows = list(reversed([126**i for i in range(np)]))
-# ch = (sum(pows[i]*A[i] for i in range(np)))%MOD
-# ph = (sum(pows[i]*(ord(P[i])) for i in range(np)))%MOD
-# po = pow(126,np,MOD)
-# if ch == ph:
-#     print(0)
-# for i in range(len(T)-np):
-#     ch = (ch*126-po*A[i]+A[i+np])%MOD
-#     if ch == ph:
-#         print(i+1)
+A = [ord(c) for c in T]
+pows = [1]*(NT+1)
+shash = [0]*(NT+1)
+for i in range(NT):
+    pows[i+1] = (pows[i]*126)%MOD
+    shash[i+1] = (shash[i]*126+A[i])%MOD
+ph = 0
+for i in range(NP):
+    ph = (ph*126+ord(P[i]))%MOD
+for i in range(len(T)-NP+1):
+    if ph == (shash[i+NP]-shash[i]*pows[NP])%MOD:
+        print(i)
